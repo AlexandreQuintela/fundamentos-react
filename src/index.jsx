e9a6c9e4be2041f1a4c36b5a6c571796cec09be8
AlexandreQuintela <alexsquin@gmail.com>
2021-12-09 07:16:52 -0300

    Criado o componente de fragmento e formatacao de todos os arquivos

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 const conteudo = <p>Meu conteúdo maravilhoso!</p>
 //const el = document.getElementById('root') // ----> pode colocar elemento dentro de euma constante
@@ -25,5 +26,7 @@ ReactDom.render(
             aluno="Maria" 
             disciplina="Maravilha"
             nota={9.9}/>
+
+            <Fragmento />            
         </div>, document.getElementById('root') //el // ---> usar a constante como parametro
 )
